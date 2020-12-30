bd877b18b6054dbc8bd38a125fbb4571dc63681c
psick22 <kgirok@gmail.com>
2020-12-30 23:54:58 +0900

    Add loading animation

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,17 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
+
   const inputRef = useRef();
+
   const onButtonClick = e => {
     e.preventDefault();
     inputRef.current.click();
   };
+
   const onChange = async event => {
+    setLoading(true);
     const upload = await imageUploader.upload(event.target.files[0]);
-    console.log(upload);
+    setLoading(false);
     onFileChange({ name: upload.original_filename, url: upload.url });
   };
+
   return (
     <div className={styles.container}>
       <input
@@ -22,9 +28,15 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         name="file"
         onChange={onChange}
       />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || 'No File'}
-      </button>
+      {!loading && (
+        <button
+          className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+          onClick={onButtonClick}
+        >
+          {name || 'No File'}
+        </button>
+      )}
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
