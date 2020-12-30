048d0fd6370318d215c84ce021b2a8f819aa9356
psick22 <kgirok@gmail.com>
2020-12-30 09:54:27 +0900

    Add preview style

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import styles from './card.module.css';
 
+const DEFAULT_IMAGE = '/images/default_logo.png';
 const Card = ({ card }) => {
-  console.log(card);
-  return <h1>{card.name}</h1>;
+  const {
+    name,
+    company,
+    title,
+    email,
+    message,
+    theme,
+    fileName,
+    fileURL,
+  } = card;
+  const url = fileURL || DEFAULT_IMAGE;
+  return (
+    <li className={`${styles.card} ${getTheme(theme)}`}>
+      <img className={styles.profile} src={url} alt="profile" />
+      <div className={styles.info}>
+        <h1 className={styles.name}>{name}</h1>
+        <p className={styles.company}>{company}</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.email}>{email}</p>
+        <p className={styles.message}>{message}</p>
+      </div>
+    </li>
+  );
 };
+function getTheme(theme) {
+  switch (theme) {
+    case 'dark':
+      return styles.dark;
+    case 'light':
+      return styles.light;
+    case 'colorful':
+      return styles.colorful;
+    default:
+      throw new Error(`unknown theme : ${theme}`);
+  }
+}
 
 export default Card;
